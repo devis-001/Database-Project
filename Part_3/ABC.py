dca65de530b890a16b9860ff84c6cca993af82f9
jacqeinz <jacquicv.19@gmail.com>
2023-03-22 00:05:06 -0600

    corrections to .py, working on queries, made notes

--- Part_3/ABC.py
+++ Part_3/ABC.py
@@ -1,4 +1,5 @@
 import sqlite3
+from sqlite3 import Error
 import sys
 
 sys.builtin_module_names
@@ -8,15 +9,89 @@ def create_connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
+        cursor=conn.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        print("Connection Established " + sqlite3.version)
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
-
+    return conn
+    
 
 if __name__ == '__main__':
     create_connection(r"ABC.sqlite")
+    
+
+
+
+def close_connection(conn):
+     conn.close
+     print("The connection with the database has been closed. ")
+
+"""
+insert: define functions here
+"""
+
+def select_questionOne(conn, addressInput):
+    """
+    Query Sites by address
+    :param conn: the connection object
+    :param addressInput: Address given in query
+    :return:
+    """
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM Sites WHERE address = ?", (addressInput,))
+    rows = cur.fetchall()
+
+    for row in rows:
+         print(row)
+         
+    print("Operation done successfully \n")
+    close_connection(conn)
+   
+def select_questionTwo(conn, schedularSystemInput):
+    """
+    Query DigitalDisplay, Specializes, TechnicalSupport by schedularSystem
+    :param conn: the connection object
+    :param schedularSystemInput: schedularSystem entered in query
+    :return:
+    """
+    cur=conn.cursor()
+    cur.execute("SELECT serialNo, modelNo, names FROM DigitalDisplay, Specializes, TechnicalSupport WHERE DigitalDisplay.modelNo=Specializes.modelNo AND Specializes.empId=TechnicalSupport.empId AND schedularSystemInput = ?", (schedularSystemInput,))
+    rows = cur.fetchall()
+
+    for row in rows:
+         print(row)
+         
+    print("Operation done successfully \n")
+    close_connection(conn)
+
+def select_questionThree(conn):
+    """
+    Query DigitalDisplay, Specializes, TechnicalSupport 
+    :param conn: the connection object
+    :return:
+    """
+
 
 def main():
+    database = (r"ABC.sqlite")
+    conn= create_connection(database)
+   
+    if __name__ == '__main__':
+        create_connection(r"ABC.sqlite")
+    
+    print("Connection with database has been established")
+  
+    with conn:
+        while( len(sys.argv) > 3):
+            s1, s2 = sys.argv[1], sys.argv[2]
+
+            """
+            Insert call statements here
+            """
+
+
+if __name__ == '__main__':
+    main()
+    
+
