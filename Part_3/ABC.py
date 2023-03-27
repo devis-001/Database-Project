a83b7308c7e876b309c27dbaacf68e1b799ca92a
jacqeinz <jacquicv.19@gmail.com>
2023-03-26 22:38:00 -0600

    All queries working. Main function working.

--- Part_3/ABC.py
+++ Part_3/ABC.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 import sys
+import argparse
 
 sys.builtin_module_names
 
@@ -16,8 +17,8 @@ def create_connection(db_file):
         print(e)
     return conn
     
-if __name__ == '__main__':
-    create_connection(r"Part_3/ABC.sqlite")
+# if __name__ == '__main__':
+#     create_connection(r"Part_3/ABC.sqlite")
     
 
 def close_connection(conn):
@@ -28,24 +29,34 @@ def close_connection(conn):
 insert: define functions here
 """
 
-def select_questionOne(conn, addressInput):
+def select_questionOne(conn, other):
     """
     Query Sites by address
     :param conn: the connection object
-    :param addressInput: Address given in query
+    :param other: Address given in query
     :return:
     """
+    print("in question one")
+ 
     cur=conn.cursor()
-    cur.execute("SELECT * FROM Sites WHERE address = ?", (addressInput,))
-    rows = cur.fetchall()
 
-    for row in rows:
-         print(row)
-         
-    print("Operation done successfully \n")
-    close_connection(conn)
+    wrapped_other = ("%%"+other+"%%",)
+
+    cur.execute("SELECT * FROM Site WHERE address LIKE ?", wrapped_other)
+    
+    records = cur.fetchall()
+
+
+    if((len(records)) != 0):
+        for row in records: 
+            print(row)
+    else:
+        print ("No matching rows returned.") 
    
-def select_questionTwo(conn, schedularSystemInput):
+    
+    close_connection(conn)
+
+def select_questionTwo(conn, schedulerSystemInput):
     """
     Query DigitalDisplay, Specializes, TechnicalSupport by schedularSystem
     :param conn: the connection object
@@ -53,38 +64,75 @@ def select_questionTwo(conn, schedularSystemInput):
     :return:
     """
     cur=conn.cursor()
-    cur.execute("SELECT serialNo, modelNo, names FROM DigitalDisplay, Specializes, TechnicalSupport WHERE DigitalDisplay.modelNo=Specializes.modelNo AND Specializes.empId=TechnicalSupport.empId AND schedularSystemInput = ?", (schedularSystemInput,))
-    rows = cur.fetchall()
+    cur.execute("SELECT serialNo, DigitalDisplay.modelNo, name FROM DigitalDisplay, Specializes, TechnicalSupport WHERE DigitalDisplay.modelNo=Specializes.modelNo AND Specializes.empId=TechnicalSupport.empId AND schedulerSystem = ?", (schedulerSystemInput,))
+    records = cur.fetchall()
 
-    for row in rows:
-         print(row)
+    if((len(records)) != 0):
+        for row in records: 
+            print(row)
+    else:
+        print ("No matching rows returned.") 
          
-    print("Operation done successfully \n")
+  
     close_connection(conn)
 
-def select_questionThree(conn):
+def select_questionThree(conn, other):
     """
     Query DigitalDisplay, Specializes, TechnicalSupport 
     :param conn: the connection object
     :return:
     """
+    cur=conn.cursor()
+    cur.execute("SELECT COUNT(DISTINCT name) AS cnt, name AS Name FROM Salesman WHERE (empId, name, gender) IN (SELECT empId, name, gender FROM SalesMan WHERE Name = Name) GROUP BY name ORDER BY name ASC") 
+    records = cur.fetchall()
+
+    if((len(records)) != 0):
+        for row in records: 
+            print(row)
+           
+    else:
+        print ("Empty Table.") 
+         
+   
+    close_connection(conn)
 
 
-def main():
+
+
+def main(question_num, other):
     database = (r"Part_3/ABC.sqlite")
     conn= create_connection(database)
-    print("This is inside the main function")
-  
-    with conn:
-        while( len(sys.argv) > 3):
-            s1, s2 = sys.argv[1], sys.argv[2]
+    print("in main, question_num = " + question_num)
+    
+    if(question_num=='1'):
+        print('calling question one')
+        if other is None:
+            #error message here
+            print("Missing parameter.")
+            return 1
+        select_questionOne(conn, other)
 
-            """
-            Insert call statements here
-            """
-
-
+    elif(question_num=='2'):
+        if other is None: 
+            print("Missing parameter.")
+            return 1
+        select_questionTwo(conn, other)
+    elif(question_num=='3'):
+        select_questionThree(conn, other)
+    else:
+        close_connection(conn)
+        return 1
+            
+        
 if __name__ == '__main__':
-    main()
+    print(sys.argv)
+    if (len(sys.argv) >= 3):
+        main(sys.argv[1], sys.argv[2])
+        if (len(sys.argv) > 3):
+            main(sys.argv[1], sys.argv[2:])
+       
+
+    else:
+        main(sys.argv[1], None)
     
 
