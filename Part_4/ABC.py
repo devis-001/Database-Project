f68664b60530041f07a609b7a52a56647e73c4e8
Mightysteve101 <Mightysteve101@gmail.com>
2023-04-28 15:39:00 -0600

    added parameters to select_option_three; added conn.commit(); added records; added lengthe of records loop

--- Part_4/ABC.py
+++ Part_4/ABC.py
@@ -64,7 +64,7 @@ def select_option_one(conn):
     '''
     def select_option_two(conn, schedulerSystem):
         cur=conn.cursor()
-        cur.execute("SELECT * FROM DigitalDisplay WHERE schedulerSystem = ?", (schedulerSystem,))
+        cur.execute("SELECT * FROM DigitalDisplay WHERE schedulerSystem = ?", schedulerSystem,)
         records = cur.fetchall()
         if((len(records)) != 0):
             for row in records:
@@ -76,10 +76,21 @@ def select_option_one(conn):
             close_connection(conn)
 
 #def select_option_three(conn):
-    def select_option_two(conn, insert):
+    def select_option_two(conn, serialNo, schedularSystem, modelNo):
         cur = conn.cursor()
-        cur.execute('INSERT INTO DigitalDisplay(serialNo, schedulerSystem, modelNo) VALUES (?, "?", ?);', (insert,))
+        cur.execute("INSERT INTO DigitalDisplay(serialNo, schedulerSystem, modelNo) VALUES (?, ?, ?)", serialNo, schedularSystem, modelNo)
+        #This method commits the current transaction. If you don't call this method, anything you did since the last call to commit() is not 
+        #visible from other database connections.
         conn.commit()
+        records = cur.fetchall()
+        if((len(records)) !=0):
+            for row in records:
+                print(row)
+        
+        else:
+            print("Empty table")
+
+            close_connection(conn)
 
 
 
