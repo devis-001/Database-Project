9045bfac95c671596be97aa3aa9e20a21bbe4381
Mightysteve101 <Mightysteve101@gmail.com>
2023-05-08 20:54:15 -0600

    figureing out binding error

--- Part_4/ABC.py
+++ Part_4/ABC.py
@@ -86,9 +86,8 @@ def select_option_two(conn):
 def select_option_three(conn):
     cur = conn.cursor()
     serialNo = input("What is the serialNo: ")
-    cur.execute("INSERT INTO DigitalDisplay(serialNo) VALUES (?)", (serialNo,));
     schedulerSystem = input("What type of schedularSystem: ")
-    cur.execute("INSERT INTO DigitalDisplay(schedulerSystem) VALUES (?)", (schedulerSystem,));
+    cur.execute("INSERT INTO DigitalDisplay(serialNo, schedulerSystem, modelNo) VALUES (?, ?)", (serialNo, schedulerSystem,))
     #modelNo = input("What is the modelNo: ")
     #cur.execute("INSERT INTO DigitalDisplay(modelNo) VALUES (?)", (modelNo,));
     # This method commits the current transaction. If you don't call this method, anything you did since the last call to commit() is not
